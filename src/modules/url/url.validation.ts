5eecc03016dd585af9da8b0bb45f9cb3c1d50c5a
fahimahammed <fahimahammed.cse@gmail.com>
2025-01-16 14:28:53 +0600

    feat: update Zod validation for originalUrl to allow only http, https, and ftp protocols

--- src/modules/url/url.validation.ts
+++ src/modules/url/url.validation.ts
@@ -1,8 +1,13 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const shortenUrlSchema = z.object({
     body: z.object({
-        originalUrl: z.string().url(),
-        customShortUrl: z.string().max(20).optional(),
+        originalUrl: z.string().url().refine((url) => {
+            const regex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+            return regex.test(url);
+        }, {
+            message: "URL must be valid and use http, https, or ftp protocols."
+        }),
+        customShortCode: z.string().max(20).optional(),
     })
 });
